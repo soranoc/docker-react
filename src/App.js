840381d37b82d76b431ef3fe2607923503042e5b
Clément SORANO <clement.sorano@acteam-it.com>
2020-11-16 10:11:44 +0100

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there !
+          Hello there feature branch !
         </p>
         <a
           className="App-link"
